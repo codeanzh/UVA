9eabbe6bc1399e9ad982c4c7b0ea4b06fb087a7f
Anzh <anzhrai@gmail.com>
2021-04-21 00:54:15 +0530

    UVA 10276

---
+++ UVA 10276.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#define ll long long int
+#define VEC vector<ll>
+#define rep(i, st, n) for (ll i = st; i < n; i++)
+#define wtc() ll tc; cin >> tc; while (tc--)
+
+bool check_prime(ll n)
+{
+    ll i = 1;
+    for (; i * i <= n; i++);
+    i--;
+    
+    return i * i == n;
+}
+
+void solve()
+{
+    wtc()
+    {
+        ll n, ans = 1;
+        cin >> n;
+        VEC pegs(n, 0);
+
+        while (true)
+        {
+            bool flag = true;
+
+            rep (i, 0, n)
+                if (pegs[i] == 0 || check_prime(pegs[i] + ans))
+                {
+                    pegs[i] = ans++;
+                    flag = false;
+                    break;
+                }
+            
+            if (flag)
+                break;
+        }
+
+        cout << ans - 1 << "\n";
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    solve();
+
+    return 0;
+}
