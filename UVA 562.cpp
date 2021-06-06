77f7355a3ee3f620acb97cfb39d0bff8e7db2555
Anzh <anzhrai@gmail.com>
2021-06-06 18:24:21 +0530

    UVA 562.cpp

--- UVA 562.cpp
+++ UVA 562.cpp
@@ -18,26 +18,21 @@ ll min_diff(VEC &arr)
     rep (i, 0, n)
         sum += arr[i];
     
-    V(V(bool)) DP(n + 1, V(bool)(sum + 1, false));
+    V(V(bool)) DP(n + 1, V(bool)((sum / 2) + 1, false));
     
     rep (i, 0, n + 1)
         DP[i][0] = true;
 
     rep (i, 1, n + 1)
-        rep (j, 1, sum + 1)
+        rep (j, 1, (sum / 2) + 1)
             if (j < arr[i - 1])
                 DP[i][j] = DP[i - 1][j];
             else
                 DP[i][j] = DP[i - 1][j] || DP[i - 1][j - arr[i - 1]];
     
-    if (sum / 2 == 0)
-        return sum;
-    else
-    {
-        repr (i, sum / 2, 0)
-            if (DP[n][i])
-                return sum - 2 * i;
-    }
+    repr (i, sum / 2, 0)
+        if (DP[n][i])
+            return sum - 2 * i;
 }
 
 void solve()
